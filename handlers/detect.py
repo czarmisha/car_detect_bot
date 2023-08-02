979c44125b5f168a91ffe5d8b890bc311ff8105e
Bobur Amirov <amirovbobur52@gmail.com>
2023-08-02 04:35:45 +0000

    added web app in docker

--- handlers/detect.py
+++ handlers/detect.py
@@ -1,4 +1,7 @@
-import os, requests, base64, logging, re
+import os
+import requests
+import logging
+import re
 from pathlib import Path
 from datetime import datetime
 from telegram import Update
@@ -6,56 +9,78 @@ from telegram.ext import CallbackContext, MessageHandler, Filters
 from sqlalchemy import select, func
 from dotenv import load_dotenv
 from db.models import Car, Session, engine
+import telebot
 
 _BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = os.path.join(_BASE_DIR, '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-_TOKEN = os.environ['PLATE_RECOGNITION_TOKEN']
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
-local_session = Session(bind=engine)
+TOKEN = os.environ['BOT_TOKEN']
+bot = telebot.TeleBot(TOKEN)
 
-def get_plate_numbers(file_path):
+
+def get_plate_numbers(photo_url):
     logger.info('API is working')
-    with open(file_path, "rb") as image:
-            img_b64 = base64.b64encode(image.read())
-            url = 'https://api.platerecognizer.com/v1/plate-reader/'
-            data={
-                'regions': 'uz',
-                'upload': img_b64,
-                "detection_rule": "strict",
-            }
-            headers = {
-                "Authorization": f"Token {_TOKEN}"
-            }
-            response = requests.post(url=url, headers=headers, data=data)
-            if response.ok:
-                return (True, response.json()['results'])
-            else:
-                return (False, response.text)
+
+    url = "https://license-plate-detection.p.rapidapi.com/license-plate-detection"
+
+    payload = {"url": photo_url}
+    headers = {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "4f4806731fmsh1202809157ba9b3p1db88fjsne2d8098201c4",
+        "X-RapidAPI-Host": "license-plate-detection.p.rapidapi.com"
+    }
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()  # Raise an exception for bad responses (4xx or 5xx)
+
+        data = response.json()
+        print(data)  # Print the API response for debugging purposes
+
+        # Extract plate numbers from the list of dictionaries
+        return data
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error while calling license plate API: {e}")
+        return []
+    except Exception as e:
+        logger.error(f"Error while processing license plate API response: {e}")
+        return []
+
+
 
 
 def car_detect(update: Update, context: CallbackContext):
     logger.info('Start detection...')
     logger.info('Downloading file')
-    file = context.bot.getFile(update.message.photo[1].file_id)
-    file_path = f'media/images/{update.effective_user.id}-{datetime.now().strftime("%Y%m%d%H%M")}.jpg'
-    file.download(file_path)
 
-    plate_nums = get_plate_numbers(file_path)
-    if not plate_nums[0]:
+    photo = update.message.photo[-1]
+    file = bot.get_file(photo.file_id)
+    file_path = file.file_path
+    url = f'https://api.telegram.org/file/bot{TOKEN}/{file_path}'
+
+    plate_nums = get_plate_numbers(url)
+    if not plate_nums:
         logger.info('I can\'t recognize the plate number')
-        os.remove(file_path)
-        return #ignore if does not recognize plate on image
-    for num in plate_nums[1]:
-        logger.info(f"FINDING CAR from db. Plate num: {num['plate']}")
-        statement = select(Car).filter(func.similarity(Car.plate, num['plate'].upper()) > 0.4)
+        return  update.message.reply_text('did not recognize the plate number')
+
+    local_session = Session(bind=engine)
+    for num in plate_nums:
+        l_pic = num['value']
+        l5_pic = l_pic[-5:]
+
+
+        logger.info(f"KUKU {select(Car).filter(func.substr(Car.plate, 4, 5))} {type(Car.plate)}")
+
+        statement = select(Car).filter(func.similarity(func.substr(Car.plate, 4, 5), l5_pic.upper()) > 0.3)
+
         car = local_session.execute(statement).scalars().first()
         if car:
             phone = re.sub("[^0-9]", "", car.owner_phone)
@@ -65,19 +90,26 @@ def car_detect(update: Update, context: CallbackContext):
                 phone = f'+{phone}'
             logger.info('SUCCESS DETECTION')
             txt = 'Это возможно наша машина:\n'
-            txt +=  f'Номер машины: {car.plate}\n' if car.plate else ''
-            txt +=  f'Имя: {car.owner_name}' if car.owner_name else ''
-            txt +=  f' - {car.owner_username}\n' if car.owner_username else ''
-            txt +=  f'Номер владельца: {phone}\n' if phone else ''
-            txt +=  f'Отдел: {car.owner_department}\n' if car.owner_department else ''
-            txt +=  f'Кабинет: {car.owner_cabinet}\n' if car.owner_cabinet else ''
+            txt += f'Номер машины: {car.plate}\n' if car.plate else ''
+            txt += f'Имя: {car.owner_name}' if car.owner_name else ''
+            txt += f'- {car.owner_username}\n' if car.owner_username else ''
+            txt += f'Номер владельца: {phone}\n' if phone else ''
+            txt += f'Отдел: {car.owner_department}\n' if car.owner_department else ''
+            txt += f'Кабинет: {car.owner_cabinet}\n' if car.owner_cabinet else ''
+
 
             update.message.reply_text(txt)
             # if not car.owner_username:
             #     context.bot.send_contact(update.effective_chat.id, phone, car.owner_name)
-            
+        #else:
+            #logger.info('else')
+            #plt_num=num['value']
+            #domen=os.environ['BOT_DOMAIN']
+            #txt2 = f'Не удалось распознать номер ( {plt_num} ) \n(совет: сфоткать номер машины спереди чтобы номер был четко виден)\nДругой возможный вариант: этот автомобиль не зарегистрирован в нашей базе.\nЕсли это ваш автомобиль и данные о вашем автомобиле не существует в базе данных, просим перейти на сайт {domen} и внести данные о вашем автомобиле туда!\n'
+            #update.message.reply_text(txt2)
             break
+    local_session.close()
 
-    os.remove(file_path)
 
 detect_handler = MessageHandler(Filters.photo, car_detect)
+
